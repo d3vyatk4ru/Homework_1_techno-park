370dc4f59196293073b8795b03bb84a436a87c16
Daniil Devyatkin <70203689+d3vyatk4ru@users.noreply.github.com>
2021-10-14 18:12:25 +0300

    Update test_split_logic.cpp

--- Homework_1/test_split_logic.cpp
+++ Homework_1/test_split_logic.cpp
@@ -13,11 +13,13 @@ constexpr unsigned NUM = 10;
 // проверка корректности логики разделения накладных по цене
 TEST(LOGIC_TEST, Split_data_price) {
 
-    waybill_structure *waybill = new waybill_structure[NUM];
+    waybill_structure *waybill;
+    waybill = (waybill_structure *) malloc(NUM * sizeof(waybill_structure));
 
-    waybill_structure *new_waybill_1 = new waybill_structure[NUM];
+    waybill_structure *new_waybill_1, *new_waybill_2;
+    new_waybill_1 = (waybill_structure *) malloc(NUM * sizeof(waybill_structure));
 
-    waybill_structure *new_waybill_2 = new waybill_structure[NUM];
+    new_waybill_2 = (waybill_structure *) malloc(NUM * sizeof(waybill_structure));
 
     // правильный ответ
     double correct_waybill_1[5] = {10, 7, 5, 4, 2};
@@ -30,7 +32,7 @@ TEST(LOGIC_TEST, Split_data_price) {
     }
 
     unsigned int num_1 = waybill_split(waybill, new_waybill_1, new_waybill_2, NUM, 0);
-    delete[] waybill;
+    free(waybill);
 
     // проверка на кол-во накладных
     EXPECT_EQ(num_1, 5);
@@ -44,18 +46,20 @@ TEST(LOGIC_TEST, Split_data_price) {
         EXPECT_EQ(new_waybill_2[i].num, correct_waybill_2[i]);
     }
 
-    delete[] new_waybill_1;
-    delete[] new_waybill_2;
+    free(new_waybill_1);
+    free(new_waybill_2);
 }
 
 // проверка корректности логики разделения накладных по цене
 TEST(LOGIC_TEST, Split_data_weight) {
 
-    waybill_structure *waybill = new waybill_structure[NUM];
+    waybill_structure *waybill;
+    waybill = (waybill_structure *) malloc(NUM * sizeof(waybill_structure));
 
-    waybill_structure *new_waybill_1 = new waybill_structure[NUM];
+    waybill_structure *new_waybill_1, *new_waybill_2;
+    new_waybill_1 = (waybill_structure *) malloc(NUM * sizeof(waybill_structure));
 
-    waybill_structure *new_waybill_2 = new waybill_structure[NUM];
+    new_waybill_2 = (waybill_structure *) malloc(NUM * sizeof(waybill_structure));
 
     // правильный ответ
     double correct_waybill_1[5] = {10, 7, 5, 4, 2};
@@ -68,7 +72,7 @@ TEST(LOGIC_TEST, Split_data_weight) {
     }
 
     unsigned int num_1 = waybill_split(waybill, new_waybill_1, new_waybill_2, NUM, 1);
-    delete[] waybill;
+    free(waybill);
 
     // проверка на кол-во накладных
     EXPECT_EQ(num_1, 5);
@@ -82,13 +86,15 @@ TEST(LOGIC_TEST, Split_data_weight) {
         EXPECT_EQ(new_waybill_2[i].num, correct_waybill_2[i]);
     }
 
-    delete[] new_waybill_1;
-    delete[] new_waybill_2;
+    free(new_waybill_1);
+    free(new_waybill_2);
 }
 
 // корректность сортировки по цене
 TEST(SORT_TEST, Simple_sort_price) {
-    waybill_structure *waybill = new waybill_structure[NUM];
+
+    waybill_structure *waybill;
+    waybill = (waybill_structure *) malloc(NUM * sizeof(waybill_structure));
 
     for (size_t i = 0; i < NUM; ++i) {
         waybill[i].price = i + 1;
@@ -100,12 +106,14 @@ TEST(SORT_TEST, Simple_sort_price) {
         EXPECT_EQ(waybill[i].num, NUM - i);
     }
 
-    delete[] waybill;
+    free(waybill);
 }
 
 // корректность сортировки по весу
 TEST(SORT_TEST, Simple_sort_weight) {
-    waybill_structure *waybill = new waybill_structure[NUM];
+
+    waybill_structure *waybill;
+    waybill = (waybill_structure *) malloc(NUM * sizeof(waybill_structure));
 
     for (size_t i = 0; i < NUM; ++i) {
         waybill[i].price = i + 1;
@@ -117,7 +125,7 @@ TEST(SORT_TEST, Simple_sort_weight) {
         EXPECT_EQ(waybill[i].num, NUM - i);
     }
 
-    delete[] waybill;
+    free(waybill);
 }
 
 int main(int argc, char **argv) {
